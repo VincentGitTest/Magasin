29085e1ae27b35b739f625cf0a2653fd42c5980a
MalikaDenis <malika.denis@hotmail.fr>
2023-06-27 15:50:21 +0200

    Produits.csv ajouté

--- Magasin.cpp
+++ Magasin.cpp
@@ -5,6 +5,8 @@ Definition Class Magasin
 #include <vector>
 #include "Magasin.hpp"
 #include "Produit.hpp"
+#include <sstream>
+#include <fstream>
 
 
 
@@ -37,7 +39,7 @@ Magasin* Magasin::getMagasin()
 	return NULL;
 
 };
-
+//destructeur Magasin
 Magasin::~Magasin()
 {
 
@@ -48,8 +50,73 @@ Magasin::Magasin()
 
 };
 
+//insertion produit manuellement
 void Magasin::insertProduit(Produit *produit)
 {
 	listeProduits.push_back(*produit);
 }
 
+void recupererProduitExterne()
+{
+    // Recuperation depuis le fichier voulu
+    std::string fname = "Produits.csv";
+
+    std::vector<std::vector<std::string>> content;
+    std::vector<std::string> row;
+    std::string line, word;
+
+    std::fstream file(fname, std::ios::in);
+
+
+    // Recuperation des lignes
+    if (file.is_open())
+    {
+        while (getline(file, line))
+        {
+            row.clear();
+
+            std::stringstream str(line);
+
+            while (std::getline(str, word, ','))
+
+                row.push_back(word);
+            content.push_back(row);
+        }
+    }
+    else
+    {
+        std::cout << "Could not open the file\n";
+    }
+
+
+
+    // Listes de recuperation des noms et des prix
+    std::vector<std::string> noms;
+    std::vector<int> prix;
+
+
+    // Lecture des lignes
+    for (int i = 0; i < content.size(); i++)
+    {
+        for (int j = 0; j < content[i].size(); j++)
+        {
+            if (i == 0)
+            {
+                noms.push_back(content[i][j]);
+            }
+            if (i == 1)
+            {
+                prix.push_back(stoi(content[i][j]));
+            }
+        }
+    }
+
+    for (int i = 0; i < noms.size(); i++)
+    {
+        Produit* produit = new Produit(noms[i], prix[i]);
+
+        Magasin::insertProduit(produit);
+
+    }
+
+}
